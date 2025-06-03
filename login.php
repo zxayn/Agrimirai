f0557439e7a4cf0af40b7f930317ce636718df39
redemtusjejeg@gmail.com <zidanxjr8@gmail.com>
2025-06-04 00:02:20 +0700

    ggg

--- login.php
+++ login.php
@@ -515,7 +515,7 @@ header .fa-bars {
 
       <!-- Regis -->
       <div class="d-grid gap-2" style="text-align: right;">
-        <a href="registrasi.php" class="register-link">Belum punya akun? Daftar Sekarang</a>
+        <a href="registrasi.php" class="register-link">Belum punya akun? Daftar Sekarangggggg</a>
       </div>
 
       <!-- Submit -->
