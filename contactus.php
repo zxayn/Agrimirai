8c547c9cd13a2d01a868b18509a3532409bfcfe6
redemtusjejeg@gmail.com <zidanxjr8@gmail.com>
2025-06-08 03:59:35 +0700

    08/06/2025

--- contactus.php
+++ contactus.php
@@ -2,10 +2,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <title>Contact | AgriMirai</title>
+    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <!-- <link rel="stylesheet" href="stylec.css"/> -->
+    <link rel="shortcut icon" href="img/logo.png">
 
 </head>
 
@@ -422,6 +423,7 @@ body {
 <!-- NAVBAR END -->
 
 <!--Jumbotron-->
+
 <div class="hero">
     <div class="overlay"></div>
     <div class="hero-content">
